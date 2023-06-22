5aeced5190480b446a78faf13439a770d86c1785
ggunG <ggung0929@gmail.com>
2023-06-22 18:26:51 +0900

    6.22 수정기능

--- community/views.py
+++ community/views.py
@@ -5,20 +5,23 @@ from community.forms import PostForm
 from community.models import Post
 
 
-def community(request):     # post 리스트 페이지
+# post 리스트 페이지
+def community(request):
     post_list = Post.objects.order_by('-create_date')   # 내림차순, 새로운 글이 맨위로
     context = {'post_list': post_list}                  # post 리스트 가져오기
     return render(request, 'community/community.html', context)     # 가져와서, 템플릿에 연결, 가져와야 할 것
 
 
-def detail(request, post_id):   # post 상세보기(post_id 필요)
+# post 상세보기(post_id 필요)
+def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)  # post_id를 pk로 함
     context = {'post': post}    # post 가져오기
     return render(request, 'community/detail.html', context)
 
 
+# post 생성하기
 @login_required(login_url='common:login')   # 로그인요구-로그인페이지연결
-def post_create(request):   # post 생성하기
+def post_create(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():     # 폼이 유효하면
@@ -32,8 +35,26 @@ def post_create(request):   # post 생성하기
     return render(request, 'community/post_form.html', context)
 
 
+# post 수정하기(post_id 필요)
 @login_required(login_url='common:login')
-def post_delete(request, post_id):  # post 삭제하기(post_id필요)
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id, writer=request.user)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.modify_date = timezone.now()
+            post.save()
+            return redirect('community/post_edit.html', post_id=post_id)
+    else:
+        form = PostForm(instance=post)
+    context = {'form': form}
+    return render(request, 'community/post_form.html', context)
+
+
+# post 삭제하기(post_id 필요)
+@login_required(login_url='common:login')
+def post_delete(request, post_id):
     post = get_object_or_404(Post, pk=post_id)  # post_id를 pk로 함
     post.delete()                               # 삭제하기(장고명령어)
     return redirect('community:community')      # 목록페이지로 넘어가기
