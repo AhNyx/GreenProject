2722d976f5290d453e908d6ee8ec83bedfd23568
Jihye Kwon <ggung0929@gmail.com>
2023-06-25 13:40:37 +0900

    6.24 사이드바grid지정, css조금(a,카테고리부분), 조회수구현

--- community/models.py
+++ community/models.py
@@ -21,15 +21,26 @@ class Category(models.Model):
 
 class Post(models.Model):   # 포스트 모델
     title = models.CharField(max_length=200)    # 제목: Char필드는 제한필요(최대=한글100자)
-    content = models.TextField()                # 내용
     writer = models.ForeignKey(User, on_delete=models.CASCADE)  # 글쓴이: FK, User모델에서 가져옴 삭제연동
     create_date = models.DateTimeField(auto_now_add=True)       # 생성일: 최초 생성시 자동생성, 수정시 변경안됨
     modify_date = models.DateTimeField(null=True, blank=True)   # 수정일: 수동생성(null/공백 가능)
-    views = models.IntegerField(default=0)      # 조회수
-    likes = models.IntegerField(default=0)      # 좋아요
-    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)    # 카테고리: FK(null/공백 가능, 삭제비연동)
-    attach = models.ImageField(upload_to='community/images/%Y/%m/%d/', null=True, blank=True)   # 첨부(저장폴더, null/공백 가능)
-    description = RichTextUploadingField(null=True, blank=True)     # 편집기 연결
+    views = models.PositiveIntegerField(default=0)      # 조회수 - 양수만 입력가능하도록 positive 추가
+    likes = models.PositiveIntegerField(default=0)      # 좋아요
+    # 카테고리: FK(null/공백 가능, 삭제비연동)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    # 내용 - 편집기 사용시 삭제해도 됨
+    content = models.TextField()
+    # 첨부(저장폴더, null/공백 가능) - 편집기 사용시 삭제해도 됨
+    attach = models.ImageField(upload_to='community/images/%Y/%m/%d/', null=True, blank=True)
+    # 편집기 연결 - 유효성검사를 위해서 중복가능을 해제해야함. 현재 입력된 데이터들 삭제하고 진행예정)
+    description = RichTextUploadingField(null=True, blank=True)
 
     def __str__(self):
         return self.title   # Post를 식별할 상황에서 기본인 id가 아니라 title값을 불러와줘서 알아보기 쉽게 함
+
+    # 주훈씨 모델링 참고함. @property: 메서드를 속성처럼 사용할 수 있도록 해주는 데코레이터
+    @property
+    def update_views(self):
+        self.views += 1
+        self.save()
+        return self.views
