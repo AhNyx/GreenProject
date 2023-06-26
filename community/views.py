c992d7deaf1204736b039290c458e8f13f35d479
Jihye Kwon <ggung0929@gmail.com>
2023-06-26 19:57:48 +0900

    6.26 카테고리보기에도 페이지처리, 추천 기능 보완

--- community/views.py
+++ community/views.py
@@ -29,7 +29,8 @@ def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)  # post_id를 pk로 함
     post.update_views                       # 조회수 증가
     categories = Category.objects.all()     # 카테고리 추가
-    context = {'post': post, 'categories': categories}    # 업데이트한 정보들 전달하기
+    has_liked = request.user in post.liked_user.all() if request.user.is_authenticated else False
+    context = {'post': post, 'categories': categories, 'has_liked': has_liked}    # 업데이트한 정보들 전달하기
     return render(request, 'community/detail_ck.html', context)
 
 
@@ -82,19 +83,24 @@ def category_page(request, slug):
     filter_list = Post.objects.filter(category=current_category)    # 현재 카테고리로 post 필터링
     post_list = filter_list.order_by('-create_date')    # 필터링된 post를 생성일 기준 내림차순 정렬
     categories = Category.objects.all()     # 전체 카테고리 정보
-    context = {'current_category': current_category, 'post_list': post_list, 'categories': categories}
+    page = request.GET.get('page', '1')     # 페이지 처리 시작 - 여기에서도 해주어야 start_index를 받아 올 수 있음
+    paginator = Paginator(post_list, 10)    # 장고내장모듈을 통해 페이지처리. 10개가 1페이지
+    page_obj = paginator.get_page(page)
+    context = {'current_category': current_category, 'post_list': page_obj, 'categories': categories}
     return render(request, 'community/community.html', context)
 
 
 @login_required(login_url='common:login')
 @require_POST
 def post_like(request):
-    pk = request.POST.get('pk', None)
+    pk = request.POST.get('pk')
     post = get_object_or_404(Post, pk=pk)
     user = request.user                 # 유저: 현재 로그인한 유저
     if post.liked_user.filter(id=user.id).exists():  # 추천인 중에 유저가 존재할 때(이미 추천했을 때)
         post.liked_user.remove(user)    # liked_user에서 유저 제거
+        liked = False
     else:   # 추천인 중에 유저가 없을 때(추천하지 않았을 때)
         post.liked_user.add(user)            # liked_user에 유저 추가
-    context = {'likes_count': post.count_liked_user()}   # 해당 포스트 추천인 수 전달
+        liked = True
+    context = {'likes_count': post.count_liked_user(), 'liked': liked}   # 해당 포스트 추천인 수 전달
     return HttpResponse(json.dumps(context), content_type='application/json')   # 정보를 json형태로 반환
