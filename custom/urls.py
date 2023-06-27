415d38e765950649c56285f301f0a09eae86c868
Sunggilcookies <rabbitsg1234@naver.com>
2023-06-27 14:54:45 +0900

    6.26

--- custom/urls.py
+++ custom/urls.py
@@ -5,10 +5,15 @@ app_name = 'custom'
 
 urlpatterns = [
     #http://127.0.0.1:8000/custom/
-    path("", views.main, name='main'),
-    #qna list
+    #qnalist (qna게시판)
     path("qnalist/", views.qnalist, name='qnalist'),
     path("qnalist2/", views.qnalist2, name='qnalist2'),
     path("qnalist3/", views.qnalist3, name='qnalist3'),
+
+    #1:1 게시판 - help
+    path("helplist", views.help_list, name='help_list'), #목록
+    path('helplist/<int:help_id>/', views.help_view, name='help_view'),  #상세 페이지
+    path('helpwrite/', views.help_write, name='help_write'),        # 1:1상담 form 페이지
+    path('helpmodify/<int:help_id>/', views.help_modify, name='help_modify'),     # 1:1상담 수정
 
 ]
