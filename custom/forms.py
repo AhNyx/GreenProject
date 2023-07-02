593fb0a8027d998f52dddafc86880af1eec9cf58
Sunggilcookies <rabbitsg1234@naver.com>
2023-07-02 13:58:25 +0900

    최종본

--- custom/forms.py
+++ custom/forms.py
@@ -1,36 +1,29 @@
 from django import forms
-from custom.models import Help, Answer
+from custom.models import Help,Notice
 
 
 # 포스트 폼 생성
 class HelpForm(forms.ModelForm):
     class Meta:
         model = Help  # Help 객체 생성
-        fields = ['title', 'email', 'content', ]
+        fields = ['title', 'email', 'content', 'answer']
         labels = {
             'title': '제목',
             'email': '이메일',
             'content': '내용',
-
+            'answer': '답변'
 
         }
-
-class AnswerForm(forms.ModelForm):
+        exclude = ['views']
+class NoticeForm(forms.ModelForm):
     class Meta:
-        model = Answer
-        fields = ['title', 'content']
+        model = Notice # Notice 객체 생성
+        fields = ['title',  'content' ]
         labels = {
             'title': '제목',
-            'content': '답변 내용',
+            'content': '내용',
+
         }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['title'].widget.attrs['readonly'] = True
-        self.fields['title'].initial = f"Re: {self.instance.question.title}"
-
-    def save(self, commit=True):
-        self.instance.title = f"Re: {self.instance.question.title}"
-        return super().save(commit)
 
 
--- custom/urls.py
+++ custom/urls.py
@@ -11,17 +11,21 @@ urlpatterns = [
     path("qnalist3/", views.qnalist3, name='qnalist3'),
 
     # 1:1 게시판 - help
-    path("helplist/", views.help_list, name='help_list'),  # 목록
-    path('helplist/<int:help_id>/', views.help_view, name='help_view'),  # 상세 페이지
-    path('helpwrite/', views.help_write, name='help_write'),  # 1:1상담 form 페이지
-    path('helpmodify/<int:help_id>/', views.help_modify, name='help_modify'),  # 1:1상담 수정
-    path('helpdelete/<int:help_id>/', views.help_delete, name='help_delete'),  # 글삭제
+    path("help/list/", views.help_list, name='help_list'),  # 목록
+    path('help/list/<int:help_id>/', views.help_view, name='help_view'),  # 상세 페이지
+    path('help/write/', views.help_write, name='help_write'),  # 1:1상담 form 페이지
+    path('help/modify/<int:help_id>/', views.help_modify, name='help_modify'),  # 1:1상담 수정
+    path('help/delete/<int:help_id>/', views.help_delete, name='help_delete'),  # 글삭제
+    path('help/answer/create/<int:help_id>/', views.help_answer, name='help_answer'),  # 답변 작성
+    path('help/answer/modify/<int:help_id>/', views.answer_modify, name='answer_modify'),  # 답변 수정
+    path('help/answer/delete/<int:help_id>/', views.answer_delete, name='answer_delete'),  # 답변 삭제
 
-
-    # 1:1 게시판 - help
-    path('aview/<int:help_id>/', views.help_aview, name='help_aview'),  # 답변상세 페이지
-    path('answer/<int:help_id>/', views.help_answer, name='help_answer'),  #답변작성
 
     # 공지사항
-    path("helpnotice/", views.help_notice, name='help_notice'),  # 목록
+    path("notice/list/", views.notice_list, name='notice_list'),  # 공지목록
+    path('notice/list/<int:notice_id>/', views.notice_view, name='notice_view'),  # 상세 페이지
+    path('notice/write/', views.notice_write, name='notice_write'),  # 공지작성 페이지
+    path('notice/modify/<int:notice_id>/', views.notice_modify, name='notice_modify'),  # 공지 수정
+    path('notice/delete/<int:notice_id>/', views.notice_delete, name='notice_delete'),  # 공지사항삭제
+
 ]
